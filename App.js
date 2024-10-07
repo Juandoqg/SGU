5e558cb66d1f814ad0e3f348dac363c3342a4635
Juandoqg <juandoqg@gmail.com>
2024-10-07 11:54:49 -0500

    Estructura del proyecto

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React from 'react';
+import { View, Text, StyleSheet, Alert } from 'react-native';
+import CustomButton from './src/components/customButton';
+
 
 export default function App() {
+  // Función que se ejecuta cuando se presiona el botón
+  const handleButtonPress = () => {
+    Alert.alert('¡Botón presionado!', 'Has presionado el botón.');
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>¡Bienvenido a mi App!</Text>
+
+      {/* Aquí usamos el componente CustomButton */}
+      <CustomButton title="Presiona aquí" onPress={handleButtonPress} />
     </View>
   );
 }
 
+// Estilos de la pantalla principal
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
   },
 });
