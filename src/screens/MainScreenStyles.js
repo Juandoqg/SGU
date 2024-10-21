4f2d3004314c22416613c54aab3eb6a68d2e2cff
Juan_Arturo <juanpabloarturoazcarate2@gmail.com>
2024-10-21 15:24:13 -0500

    Eliminacion code no usado

--- src/screens/MainScreenStyles.js
+++ src/screens/MainScreenStyles.js
@@ -1,36 +1,12 @@
 import { StyleSheet } from 'react-native';
 
-
 const MainScreenStyles = StyleSheet.create({
   scrollContainer: {
     flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center',
     width: 'auto',
-  },/*
-  header: {
-    alignSelf: 'stretch',
-    height: 110,
-    width: 'all',
-    padding: 32,
-    backgroundColor: 'white',
-    borderBottomWidth: 1,
-    borderBottomColor: '#D9D9D9',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
   },
-  headerIcon: {
-    height: 35,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  headerIconInner: {
-    width: 23.33,
-    height: 35,
-    borderWidth: 3.5,
-    borderColor: '#1E1E1E',
-  },*/
   mainSection: {
     
     paddingHorizontal: 24,
@@ -93,64 +69,7 @@ const MainScreenStyles = StyleSheet.create({
     color: '#F5F5F5',
     fontSize: 16,
   },
- /* image: {
-    alignSelf: 'stretch',
-    height: 400,
-  },
-  footer: {
-    width: '100%',
-    padding: 32,
-    backgroundColor: 'white',
-    borderTopWidth: 1,
-    borderTopColor: '#D9D9D9',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  footerColumn: {
-    width: 300,
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-  },
-  footerIcon: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  footerIconInner: {
-    width: 23.33,
-    height: 35,
-    borderWidth: 3.5,
-    borderColor: '#1E1E1E',
-  },
-  footerIconsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    marginTop: 24,
-  },
-  footerSquare: {
-    width: 24,
-    height: 24,
-    backgroundColor: '#1E1E1E',
-    marginHorizontal: 8,
-  },
-  footerRectangle: {
-    width: 24,
-    height: 18,
-    backgroundColor: '#1E1E1E',
-    marginHorizontal: 8,
-  },
-  footerTitle: {
-    fontWeight: '600',
-    fontSize: 16,
-    color: '#1E1E1E',
-    marginBottom: 10,
-  },
-  footerText: {
-    fontWeight: '400',
-    fontSize: 16,
-    color: '#1E1E1E',
-    marginBottom: 10,
-  },*/
+ 
 });
 
 export default MainScreenStyles;
