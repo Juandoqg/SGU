56910384d8cc33ade0f8fbf3a3e2b4c9db706a67
Jhoness11 <jhonstevengarcia111@gmail.com>
2024-10-24 15:56:11 -0500

    Auth de firebase ok

--- App.js
+++ App.js
@@ -1,65 +1,66 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SignInScreen from './src/screens/SignInScreen';
-import SignUpScreen from './src/screens/SignUpScreen';
-import MainScreen from './src/screens/MainScreen';
-import SecondScreen from './src/screens/SecondScreen';
-import PerfilScreen from './src/screens/PerfilScreen';
-import EstadoReporteScreen from './src/screens/EstadoReporteScreen';
-import ReportarScreen from './src/screens/ReportarScreen';
-import InfoAdicionalScreen from './src/screens/InfoAdicionalScreen';
-const Stack = createNativeStackNavigator();
+  import React from 'react';
+  import { NavigationContainer } from '@react-navigation/native';
+  import { createNativeStackNavigator } from '@react-navigation/native-stack';
+  import SignInScreen from './src/screens/SignInScreen';
+  import SignUpScreen from './src/screens/SignUpScreen';
+  import MainScreen from './src/screens/MainScreen';
+  import SecondScreen from './src/screens/SecondScreen';
+  import PerfilScreen from './src/screens/PerfilScreen';
+  import EstadoReporteScreen from './src/screens/EstadoReporteScreen';
+  import ReportarScreen from './src/screens/ReportarScreen';
+  import InfoAdicionalScreen from './src/screens/InfoAdicionalScreen';
+  import { appFirebase, auth } from './credenciales';
+  const Stack = createNativeStackNavigator();
 
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="MainScreen">
-        <Stack.Screen 
-          name="SignIn" 
-          component={SignInScreen} 
-          options={{ headerShown: false }} // Oculta el header por defecto
-        />
-        <Stack.Screen 
-          name="SignUp" 
-          component={SignUpScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen 
-          name="MainScreen" 
-          component={MainScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen 
-          name="SecondScreen" 
-          component={SecondScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen 
-          name="PerfilScreen" 
-          component={PerfilScreen}
-          options={{ headerShown: false }}
-        />
-         <Stack.Screen 
-          name="ReportarScreen" 
-          component={ReportarScreen}
-          options={{ headerShown: false }}
-        />
+  const App = () => {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="MainScreen">
           <Stack.Screen 
-          name="EstadoReporteScreen" 
-          component={EstadoReporteScreen}
-          options={{ headerShown: false }}
-        />
-         <Stack.Screen 
-          name="InfoAdicionalScreen" 
-          component={InfoAdicionalScreen}
-          options={{ headerShown: false }}
-        />
-         
-         
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+            name="SignIn" 
+            component={SignInScreen} 
+            options={{ headerShown: false }} // Oculta el header por defecto
+          />
+          <Stack.Screen 
+            name="SignUp" 
+            component={SignUpScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen 
+            name="MainScreen" 
+            component={MainScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen 
+            name="SecondScreen" 
+            component={SecondScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen 
+            name="PerfilScreen" 
+            component={PerfilScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen 
+            name="ReportarScreen" 
+            component={ReportarScreen}
+            options={{ headerShown: false }}
+          />
+            <Stack.Screen 
+            name="EstadoReporteScreen" 
+            component={EstadoReporteScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen 
+            name="InfoAdicionalScreen" 
+            component={InfoAdicionalScreen}
+            options={{ headerShown: false }}
+          />
+          
+          
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  };
 
-export default App;
+  export default App;
--- credenciales.js
+++ credenciales.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,7 +17,11 @@ const firebaseConfig = {
   measurementId: "G-V734F8SQLE"
 };
 
-// Initialize Firebase
 const appFirebase = initializeApp(firebaseConfig);
 
-export default appFirebase;
+const auth = initializeAuth(appFirebase, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+
+
+export {appFirebase ,auth}
